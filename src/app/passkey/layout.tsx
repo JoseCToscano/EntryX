30d7728492bacc0a3995c151ea8266e979608fa9
Jose Toscano <josetoscano@atratopago.com>
2024-09-20 18:14:55 -0600

    without cobrado

--- src/app/passkey/layout.tsx
+++ src/app/passkey/layout.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { CorbadoProvider } from "@corbado/react";
+import StellarWallet from "~/app/passkey/components/stellar-wallet";
 
 const CORBADO_PROJECT_ID = "pro-7635031262679338570";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
+      <>
     <CorbadoProvider
       projectId={CORBADO_PROJECT_ID}
       setShortSessionCookie
@@ -16,6 +18,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       </div>
       {/*All children go here*/}
     </CorbadoProvider>
+        <StellarWallet />
+      </>
   );
 };
 
